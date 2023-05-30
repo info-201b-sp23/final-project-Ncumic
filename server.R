2467720553c7eac2a57e567af8b42b7036cf7939
Ncumic <Ncumic00@gmail.com>
2023-05-30 15:07:04 -0700

    Added library to each file while also creating server and ui functions

---
+++ server.R
@@ -0,0 +1,18 @@
+library("lubridate")
+library(shiny)
+library(ggplot2)
+library(plotly)
+library(dplyr)
+library(bslib)
+library(rsconnect)
+
+# UPDATED TABLE CODE
+earthquake_data <- read.csv('https://raw.githubusercontent.com/info-201b-sp23/exploratory-analysis-Ncumic/main/earthquake_data.csv')
+earthquake_data_modified <- select(earthquake_data, -c("title","net", "nst", "dmin", "gap", "magType", "depth"))
+
+
+server <- function(input, output) {
+
+
+
+}
---
+++ ui.R
@@ -0,0 +1,11 @@
+library(shiny)
+library(ggplot2)
+library(plotly)
+library(dplyr)
+library(bslib)
+library(rsconnect)
+
+
+ui <- navbarPage(
+
+)
