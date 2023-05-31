c9bfd4c8c46dafa70941b3e564a8e8bf1c6a4e48
Ncumic <Ncumic00@gmail.com>
2023-05-31 14:03:36 -0700

    ADDED MY VISUALIZATION AND SECTIONS FOR GRaphs

--- server.R
+++ server.R
@@ -16,6 +16,31 @@ source("Summary.R")
 
 
 server <- function(input, output) {
+  # NIKOLA SECTION
+
+  # ADD PART THAT CHANGES DATAFRAME TO ONLY HOLD WANTED MAGNITUDES
+  world_data_shape <- map_data("world")
+  world_earthquake_table <- left_join(world_data_shape, earthquake_data_modified, by = c("long" = "longitude", "lat" = "latitude"))
+
+  output$Nikola_Plot <- renderPlotly({
+
+    Earthquake_plot <- ggplot(data = world_data_shape) +
+      geom_polygon(aes(x = long,
+                       y = lat,
+                       group = group)) +
+      geom_point(data = earthquake_data_modified,
+                 aes(x = longitude, y = latitude,
+                     size = magnitude),
+                 color = "Red",
+                 shape = 21
+      ) + labs(title = "Earthquake locations & respective magnitudes",
+               x = "Longitude",
+               y = "Latitude"
+    )
+
+
+    return(Earthquake_plot)
+  })
 
 
 
--- ui.R
+++ ui.R
@@ -16,10 +16,31 @@ Viz_theme <- bs_theme_update(Viz_theme, bootswatch = "journal")
 
 # WE WILL ADD OUR INDIVIDUAL UI STUFF HERE
 
+# NIKOLA_SECTION
+Nikola_Plot_Description <- p("HELLLLLLLOOOOOOOOOOOOOOOOOOOOOOO")
+
+Nikola_Plot <- mainPanel(
+  # Make plot interactive
+  plotlyOutput(outputId = "Nikola_Plot")
+)
+
+NIKOLA_TAB <- tabPanel("Data Viz",
+                       sidebarLayout(
+                         sidebarPanel(
+                           # ADD WIDGET HERE
+
+                           Nikola_Plot_Description
+                           # can add more
+                         ),
+                         Nikola_Plot
+                       )
+)
+
 
 
 
 # MAIN UI TO CONNECT EVERYTHING
 ui <- navbarPage(
+  NIKOLA_TAB
 
 )
